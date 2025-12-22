627836941a565d522bc08bbc641dc261390d609e
aniko <s.aniko9424@gmail.com>
2025-12-22 16:25:22 +0100

    Shopping list item checkbox

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,4 +1,10 @@
-import { collection, onSnapshot, query } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  query,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../config/firebase";
 import { useParams } from "react-router";
@@ -7,12 +13,13 @@ function ShoppingList() {
   const [itemList, setItemList] = useState([]);
   const params = useParams();
   const id = params.id;
+  const collectionRef = collection(db, `shopping-list/${id}/items`);
 
   useEffect(() => {
     if (!id) {
       return;
     }
-    const collectionRef = collection(db, `shopping-list/${id}/items`);
+
     const q = query(collectionRef);
     const getDataUnsub = onSnapshot(
       q,
@@ -23,7 +30,6 @@ function ShoppingList() {
         }));
 
         setItemList(data);
-        console.log(data);
       },
       (error) => {
         console.log(error);
@@ -31,6 +37,16 @@ function ShoppingList() {
     );
     return () => getDataUnsub();
   }, [id]);
+
+  const updateProduct = async (event, id) => {
+    const docRef = doc(collectionRef, id);
+
+    try {
+      await updateDoc(docRef, { checked: event.target.checked });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -52,6 +68,7 @@ function ShoppingList() {
                     type="checkbox"
                     checked={item.checked}
                     className="form-check-input"
+                    onChange={(e) => updateProduct(e, item.id)}
                   ></input>
                   <label
                     htmlFor={item.id}
